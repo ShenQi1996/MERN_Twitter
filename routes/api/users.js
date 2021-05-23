466f90332782e64a21acb670cc4299d94b0868f9
ShenQi1996 <qisam1989@gmail.com>
2021-05-23 14:52:25 -0400

    fix some bug

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const User = require("../../models/User");
 const bcrypt = require("bcryptjs");
-const keys = require("../../confg/keys");
+// const keys = require("../../confg/keys");
 const jwt = require("jsonwebtoken");
 
 const validateRegisterInput = require("../../validation/register");

edfae064e25dda3a249b33fc5f2944c7f58fa68c
ShenQi1996 <qisam1989@gmail.com>
2021-05-23 15:01:23 -0400

    fix bugs

--- routes/api/users.js
+++ routes/api/users.js
@@ -68,7 +68,7 @@ router.post("/login", (req, res) => {
 
         jwt.sign(
           payload,
-          keys.secretOrKey,
+          // keys.secretOrKey,
           { expiresIn: 3600 },
           (err, token) => {
             res.json({
